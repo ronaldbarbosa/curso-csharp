dc9e128d84a484fb17f274687a104689690ddab2
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-01-07 11:41:01 -0300

    Matrizes: exercicios

---
+++ secao-6/comportamento-de-memoria-arras-listas/exercicio-de-fixacao-matrizes/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+string[] dim = Console.ReadLine().Split();
+int x = int.Parse(dim[0]);
+int y = int.Parse(dim[1]);
+int[,] matrix = new int[x, y];
+
+for (int i = 0; i < x; i++)
+{
+    string[] values = Console.ReadLine().Split();
+
+    for (int j = 0; j < y; j++)
+    {
+        matrix[i, j] = int.Parse(values[j]);
+    }
+}
+
+int value = int.Parse(Console.ReadLine());
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        if (value == matrix[i, j])
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Position {i},{j}:");
+            if (i > 0)
+            {
+                Console.WriteLine($"Up: {matrix[i-1, j]}");
+            }
+            if (j > 0)
+            {
+                Console.WriteLine($"Left: {matrix[i, j-1]}");
+            }
+            if (j < y - 1)
+            {
+                Console.WriteLine($"Right: {matrix[i, j+1]}");
+            }
+            if (i < x - 1)
+            {
+                Console.WriteLine($"Down: {matrix[i+1, j]}");
+            }
+        }
+        
+    }
+}
---
+++ secao-6/comportamento-de-memoria-arras-listas/exercicio-resolvido-matrizes/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int n = int.Parse(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] values = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = int.Parse(values[j]);     
+    }
+}
+
+int negativeNumbers = 0;
+
+Console.WriteLine("Main diagonal:");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        if (matrix[i, j] < 0)
+        {
+            negativeNumbers++;
+        }
+    }
+}
+
+Console.WriteLine($"\nNegative numbers = {negativeNumbers}");
