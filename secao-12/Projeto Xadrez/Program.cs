050f66a1a018665b6732b467f463f60d8dcb5847
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-19 22:40:14 -0300

    Projeto xadrez: criada a classe PosicaoXadrez

--- secao-12/Projeto Xadrez/Program.cs
+++ secao-12/Projeto Xadrez/Program.cs
@@ -6,20 +6,9 @@ namespace Projeto_Xadrez
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-                tabuleiro.InserirPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.InserirPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.InserirPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
-
-                Tela.ImprimirTabuleiro(tabuleiro);
-            }
-            catch (TabuleiroException e)
-            {
-                System.Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('a', 1);
+            System.Console.WriteLine(posicaoXadrez);
+            System.Console.WriteLine(posicaoXadrez.ToPosicao());
         }
     }
 }
---
+++ secao-12/Projeto Xadrez/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+using Projeto_Xadrez;
+
+namespace Xadrez
+{
+    public class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna}{Linha}";
+        }
+    }
+}
