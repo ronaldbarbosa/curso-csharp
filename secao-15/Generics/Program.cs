32d5d0365cdf321c9c3dd38570e7b68065a68922
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-07-10 23:46:14 -0300

    Generics, Set, Dictionary: Generics

---
+++ secao-15/Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics;
+
+PrintService<string> printService = new PrintService<string>();
+
+Console.Write("How many values? ");
+int numberOfValues = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfValues; i++)
+{
+    string value = Console.ReadLine();
+    printService.AddValue(value);
+}
+
+Console.WriteLine($"First: {printService.First()}");
+printService.Print();
