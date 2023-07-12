9b05921cfd6ba5d73ac6cc6b5888e1c4cc0f50ef
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-07-11 22:57:20 -0300

    Generics, Set, Dictionary: exercicio resolvido conjuntos

---
+++ secao-15/ExercicioResolvidoConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,26 @@
+﻿namespace ExercicioResolvidoConjuntos.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string username, DateTime instant)
+        {
+            Username = username;
+            Instant = instant;
+        }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not LogRecord) return false;
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ secao-15/ExercicioResolvidoConjuntos/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioResolvidoConjuntos.Entities;
+
+string tempPath = Path.GetTempPath();
+Console.Write("Enter file name: ");
+string fileName = Console.ReadLine();
+
+HashSet<LogRecord> records = new HashSet<LogRecord>();
+
+try
+{
+    using (StreamReader sr = File.OpenText($"{tempPath}{fileName}"))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            records.Add(new LogRecord(name, instant));
+        }
+        Console.WriteLine($"Total users: {records.Count}");
+    }
+}
+catch (IOException ex) 
+{
+    Console.WriteLine(ex.Message);
+}
