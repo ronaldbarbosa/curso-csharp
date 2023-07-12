d0b42860487926ab07a9ecabdfb62ac6dd9779cf
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-07-11 23:46:11 -0300

    Generics, Set, Dictionary: exercicio proposto conjuntos

---
+++ secao-15/ExercicioPropostoConjuntos/Program.cs
@@ -0,0 +1,29 @@
+﻿HashSet<int> courseA = new HashSet<int>();
+HashSet<int> courseB = new HashSet<int>();
+HashSet<int> courseC = new HashSet<int>();
+
+Console.Write("How many students for course A? ");
+int numberCourseA = int.Parse(Console.ReadLine());
+for  (int i = 0; i < numberCourseA; i++)
+{
+    int studentNumber = int.Parse(Console.ReadLine());
+    courseA.Add(studentNumber);
+}
+
+Console.Write("How many students for course B? ");
+int numberCourseB = int.Parse(Console.ReadLine());
+for (int i = 0; i < numberCourseB; i++)
+{
+    int studentNumber = int.Parse(Console.ReadLine());
+    courseB.Add(studentNumber);
+}
+
+Console.Write("How many students for course C? ");
+int numberCourseC = int.Parse(Console.ReadLine());
+for (int i = 0; i < numberCourseC; i++)
+{
+    int studentNumber = int.Parse(Console.ReadLine());
+    courseC.Add(studentNumber);
+}
+
+Console.WriteLine($"Total students: {courseA.Union(courseB.Union(courseC)).Count()}");
