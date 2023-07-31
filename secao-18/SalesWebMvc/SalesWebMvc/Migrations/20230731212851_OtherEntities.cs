95accc257b1fda7f9ffa1bc991f3a3ebabe4d07a
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-07-31 18:39:01 -0300

    Other entities and second migration

---
+++ secao-18/SalesWebMvc/SalesWebMvc/Migrations/20230731212851_OtherEntities.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class OtherEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    BaseSalary = table.Column<double>(type: "double precision", nullable: false),
+                    DepartamentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seller", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seller_Departament_DepartamentId",
+                        column: x => x.DepartamentId,
+                        principalTable: "Departament",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    SellerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesRecords_Seller_SellerId",
+                        column: x => x.SellerId,
+                        principalTable: "Seller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecords_SellerId",
+                table: "SalesRecords",
+                column: "SellerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seller_DepartamentId",
+                table: "Seller",
+                column: "DepartamentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesRecords");
+
+            migrationBuilder.DropTable(
+                name: "Seller");
+        }
+    }
+}
---
+++ secao-18/SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
