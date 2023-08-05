925a4358b8fe97828ae8e65514531de45dcf252a
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-08-04 22:54:17 -0300

    Delete seller

--- secao-18/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ secao-18/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -7,18 +7,18 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _selllerService;
+        private readonly SellerService _sellerService;
         private readonly DepartamentService _departamentService;
 
         public SellersController(SellerService selllerService, DepartamentService departamentService)
         {
-            _selllerService = selllerService;
+            _sellerService = selllerService;
             _departamentService = departamentService;
         }
 
         public IActionResult Index()
         {
-            var list = _selllerService.FindAll();
+            var list = _sellerService.FindAll();
             return View(list);
         }
 
@@ -33,7 +33,24 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) 
         {
-            _selllerService.Insert(seller);
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+
+            var obj = _sellerService.FindById(id.Value);
+            return obj == null ? NotFound() : View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
--- secao-18/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ secao-18/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -22,5 +22,18 @@ namespace SalesWebMvc.Services
             _context.Seller.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id) 
+        {
+            Console.WriteLine(_context.Seller.FirstOrDefault(obj => obj.Id == id));
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id) 
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
