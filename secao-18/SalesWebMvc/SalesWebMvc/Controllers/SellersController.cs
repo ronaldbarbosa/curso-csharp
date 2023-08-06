e42d47f0c9d011cc0124f3903e824ac37a6b7463
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-08-06 15:43:30 -0300

    Update seller and custom service exception

--- secao-18/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ secao-18/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -59,6 +60,41 @@ namespace SalesWebMvc.Controllers
 
             var obj = _sellerService.FindById(id.Value);
             return obj == null ? NotFound() : View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        { 
+            if (id == null) return NotFound();
+
+            var obj = _sellerService.FindById(id.Value);
+            
+            if (obj == null) return NotFound();
+
+            List<Departament> departaments = _departamentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departaments = departaments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id) return BadRequest();
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException ex) 
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException ex)
+            { 
+                return BadRequest();
+            }
         }
     }
 }
